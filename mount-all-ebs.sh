39c8e54e81f5d710e06c46a9057b7b433327870e
Gabe <gabetocci@gmail.com>
2022-09-16 22:43:58 -0400

    Update mount-all-ebs.sh
    added ansible task example
    added DEBUG mode
    removed json output (which removed two dependencies: jq and util-linux v2.27+)

--- mount-all-ebs.sh
+++ mount-all-ebs.sh
@@ -16,81 +16,123 @@
 #   - ymmv
 #
 ###########################################################################
+# ANSIBLE TASK EXAMPLE
+###########################################################################
+#- name: Run EBS mounting script
+#  script: /root/bin/mount_ebs.sh
+#  args:
+#    creates: /etc/.ansible_server_init_ebs_mounts
+#
+###########################################################################
 # dependencies
 ###########################################################################
-
+#
 # aws cli
 # authentication
 # authorization to run ec2 describe-instances and describe-volumes
-
+#
 # ebs volumes must be tagged with erp:mount_point
 # eg. { "erp:mount_point": "/my-directory" }
 
+###########################################################################
+
+DEBUG=false
 AWS_REGION=us-east-1
 FS=xfs
-PKG=yum
-
-# jq
-${PKG} install jq -y
-
-# util-linux v2.27+ required (lsblk json ouput)
 
 ###########################################################################
 # main
 ###########################################################################
 
+if [ "${DEBUG}" = true ]; then
+    echo
+    echo "DEBUG MODE - DEBUG MODE - DEBUG MODE - DEBUG MODE - DEBUG MODE - DEBUG MODE"
+fi
+
 INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 
 # volumes attached to this instance
-for VOLUMEID in $(aws ec2 describe-instances --region ${AWS_REGION} --instance-ids ${INSTANCE_ID} --query 'Reservations[0].Instances[0].BlockDeviceMappings[].[Ebs.VolumeId]' --output text) ; do 
+for VOLUMEID in $(aws ec2 describe-instances --region ${AWS_REGION} --instance-ids ${INSTANCE_ID} --query 'Reservations[0].Instances[0].BlockDeviceMappings[].[Ebs.VolumeId]' --output text) ; do
+
+    MOUNTPOINT=$(aws ec2 describe-volumes --region ${AWS_REGION} --volume-ids ${VOLUMEID} --query 'Volumes[0].Tags[?Key==`mount_point`].Value' --output text)
+    TRIMVOLUMEID=$(echo ${VOLUMEID} | tr -d '-')
 
     echo
     echo VOLUMEID=${VOLUMEID};
-
-    VOLUMENAME=$(aws ec2 describe-volumes --region ${AWS_REGION} --volume-ids ${VOLUMEID} --query 'Volumes[0].Attachments[].Device' --output text)
-    MOUNTPOINT=$(aws ec2 describe-volumes --region ${AWS_REGION} --volume-ids ${VOLUMEID} --query 'Volumes[0].Tags[?Key==`erp:mount_point`].Value' --output text)
-
-    echo VOLUMENAME=${VOLUMENAME}
     echo MOUNTPOINT=${MOUNTPOINT}
 
-    # devices that have not been mounted (no UUID) and not the root volume (nvme0n1)
-    for BLOCKDEVICE in $(lsblk -o +UUID -J | jq '.blockdevices[] | select(.uuid == null) | select(.name != "nvme0n1") | .name') ; do
+    # all block devices attached to this instance
+    for BLOCKDEVICE in $(lsblk -o name -d -n) ; do
 
-        BLOCKDEVICE=$(echo ${BLOCKDEVICE} | tr -d '"')
-        VOLUMEDEVICE=$(readlink ${VOLUMENAME})
+        FULLBLOCKDEVICE="/dev/${BLOCKDEVICE}"
+        DEVICESERIAL=$(lsblk -o SERIAL -d -n ${FULLBLOCKDEVICE})
 
-        echo BLOCKDEVICE=${BLOCKDEVICE}
-        echo VOLUMEDEVICE=${VOLUMEDEVICE}
+        # skip root volume
+        if [ "${FULLBLOCKDEVICE}" != "/dev/nvme0n1" ]; then
 
-        # if the attached volume is a device that needs to be mounted
-        if [ "${VOLUMEDEVICE}" == "${BLOCKDEVICE}" ]; then
+        # skip swap
+        SWAP=$(swapon --noheadings | awk '{print $1}')
+        if [ "${FULLBLOCKDEVICE}" != "${SWAP}" ]; then
 
-            if [ "${MOUNTPOINT}" == "swap" ]; then
+            # check this volume 
+            if [ "${DEVICESERIAL}" == "${TRIMVOLUMEID}" ]; then
 
-                echo "enable swap"
-                mkswap ${VOLUMENAME}
-                swapon ${VOLUMENAME}
-                UUID=$(blkid | grep ${BLOCKDEVICE} | awk '{print $2}' | tr -d '"')
-                echo "${UUID} swap swap defaults 0 0" >> /etc/fstab
-                break;
+                UUID=$(blkid ${FULLBLOCKDEVICE} | awk '{print $2}' | tr -d '"')
 
-            else
+                echo FULLBLOCKDEVICE=${FULLBLOCKDEVICE}
+                echo ${UUID}
 
-                echo "prepare volume mount"
-                mkfs -t ${FS} ${VOLUMENAME}
-                mkdir ${MOUNTPOINT}
-                UUID=$(blkid | grep ${BLOCKDEVICE} | awk '{print $2}' | tr -d '"')
-                echo "${UUID} ${MOUNTPOINT} ${FS} defaults,nofail 0 2" >> /etc/fstab
-                break;
-            fi;
+                 # mount devices that have not been mounted (no UUID)
+                if [ "${UUID}" == "" ]; then
+    
+                    echo "FOUND VOLUME TO MOUNT: " ${MOUNTPOINT}
+
+                        if [ "${MOUNTPOINT}" == "swap" ]; then
+                    
+                            if [ "${DEBUG}" = true ]; then
+                                echo "DEBUG MODE - ENABLE SWAP - DEBUG MODE"
+                                echo "mkswap ${FULLBLOCKDEVICE}"
+                                echo "swapon ${FULLBLOCKDEVICE}"
+                                NEWUUID=$(blkid ${FULLBLOCKDEVICE} | awk '{print $2}' | tr -d '"')
+                                echo "${NEWUUID} swap swap defaults 0 0"
+                            else
+                                echo "ENABLE SWAP"
+                                mkswap ${FULLBLOCKDEVICE}
+                                swapon ${FULLBLOCKDEVICE}
+                                NEWUUID=$(blkid ${FULLBLOCKDEVICE} | awk '{print $2}' | tr -d '"')
+                                echo "${NEWUUID} swap swap defaults 0 0" >> /etc/fstab
+                            fi
+                            break;
+                        else
+                            if [ "${DEBUG}" = true ]; then
+                                echo "DEBUG MODE - MOUNT VOLUME - DEBUG MODE"
+                                echo "mkfs -t ${FS} ${FULLBLOCKDEVICE}"
+                                echo "mkdir ${MOUNTPOINT}"
+                                NEWUUID=$(blkid ${FULLBLOCKDEVICE} | awk '{print $2}' | tr -d '"')
+                                echo "${NEWUUID} ${MOUNTPOINT} ${FS} defaults,nofail 0 2"
+                            else
+                                echo "MOUNT VOLUME"
+                                mkfs -t ${FS} ${FULLBLOCKDEVICE}
+                                mkdir ${MOUNTPOINT}
+                                NEWUUID=$(blkid ${FULLBLOCKDEVICE} | awk '{print $2}' | tr -d '"')
+                                echo "${NEWUUID} ${MOUNTPOINT} ${FS} defaults,nofail 0 2" >> /etc/fstab
+                            fi
+                            break;
+                        fi;
+
+                    fi
+                fi
+            fi
         fi
-
     done
 done
 
-echo
-echo mount all
-mount -a
+if [ "${DEBUG}" != true ]; then
+    echo
+    echo mount all
+    mount -a
+    rc=$?
+    touch /etc/.ansible_server_init_ebs_mounts
+fi 
 
-exit $?
-
+exit ${rc}
